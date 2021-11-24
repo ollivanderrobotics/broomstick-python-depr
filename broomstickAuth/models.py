e6c5e1e839c85e1643cb494214e2410e4cc401ed
310831 <practice@businessdeveloper.co.in>
2021-11-24 13:59:47 +0530

    image removed

--- broomstickAuth/models.py
+++ broomstickAuth/models.py
@@ -5,7 +5,7 @@ class Note(models.Model):
     productName = models.CharField(max_length=50, default="")
     companyName = models.CharField(max_length=50, default="")
     productDescription = models.CharField(max_length=50, default="")
-    productImage = models.ImageField(upload_to='uploads', default="")
+    # productImage = models.ImageField(upload_to='uploads', default="")
     # def __str__(self):
     #     return self.productName
     #     return self.companyName
--- broomstickAuth/serializers.py
+++ broomstickAuth/serializers.py
@@ -3,4 +3,4 @@ from .models import Note
 class NoteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Note
-        fields = ('id', 'productName', 'companyName', 'productDescription', 'productImage')
+        fields = ('id', 'productName', 'companyName', 'productDescription')
