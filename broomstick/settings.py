3fe29975e2be1c731bdf756a5ca7b3d7f40ae8bd
310831 <practice@businessdeveloper.co.in>
2021-11-24 13:55:10 +0530

    aDDING ssl Middleware

--- broomstick/settings.py
+++ broomstick/settings.py
@@ -57,6 +57,7 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.middleware.security.SecurityMiddleware',
 ]
 CORS_ALLOW_ALL_ORIGINS = True
 ROOT_URLCONF = 'broomstick.urls'
