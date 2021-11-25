3a19407a69a6a27f83480d823c61b269fec655cf
310831 <practice@businessdeveloper.co.in>
2021-11-25 14:27:38 +0530

    changed cors headers

--- broomstick/settings.py
+++ broomstick/settings.py
@@ -27,9 +27,7 @@ SECRET_KEY = 'django-insecure-lzskkce0d2&4v=$3-i#=bk-!nncjs#=g^=x5desaa!t3%egtjn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    'https://broomstick-2ede4.web.app'
-]
+ALLOWED_HOSTS = []
 
 CORS_ALLOW_ALL_ORIGINS = True
 # Application definition
