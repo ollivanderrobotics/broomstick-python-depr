78edd7c3e92f01767f5bc44c5672e30888d3e25d
ollivanderrobotics <ceo@ollivanderrobotics.com>
2022-09-05 19:59:50 +0530

    new model
    added few keys

--- broomstickAuth/models.py
+++ broomstickAuth/models.py
@@ -3,8 +3,11 @@ from django.db import models
 # Create your models here
 class Note(models.Model):
     productName = models.CharField(max_length=50, default="")
+    tagline = models.CharField(max_length=50, default="")
     companyName = models.CharField(max_length=50, default="")
     productDescription = models.CharField(max_length=50, default="")
+    productImageUrl = models.CharField(max_length=50, default="")
+    featured = models.CharField(max_length=50, default="")
     # productImage = models.ImageField(upload_to='uploads', default="")
     # def __str__(self):
     #     return self.productName
--- broomstickAuth/serializers.py
+++ broomstickAuth/serializers.py
@@ -3,4 +3,4 @@ from .models import Note
 class NoteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Note
-        fields = ('id', 'productName', 'companyName', 'productDescription')
+        fields = ('id', 'productName', 'tagline', 'companyName', 'productDescription','productImageUrl','featured')
